c2b8b0255123761f98ef05dea6a3643fc8e0405a
aripkhanova <aripxanovy@mail.ru>
2023-01-29 03:58:40 +0600

    Python Variables

--- input.py
+++ input.py
@@ -3,5 +3,5 @@ username = input("Enter username:")
 print("Username is: " + username)
 
 #2.7
-username = raw_input("Enter username:")
-print("Username is: " + username)
+#username = raw_input("Enter username:")
+#print("Username is: " + username)
